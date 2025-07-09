c4c2db6b6889c17a75d164723a9a5dfb94e4caae
Tran Vo Ngoc Truong <119861415+NgTruong624@users.noreply.github.com>
2025-07-09 15:11:35 +0700

    Update main.go

--- cmd/seeder/main.go
+++ cmd/seeder/main.go
@@ -22,7 +22,8 @@ func main() {
 	}
 
 	// Kết nối database
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
